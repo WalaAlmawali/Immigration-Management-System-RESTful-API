5b29a294cfd51fa9451b96be30ef8ec1a1863db9
Wala AL mawali <wala.salam.9888@gmail.com>
2026-06-15 09:36:32 +0400

    Implement constructor,getter and setter for Applicant class

--- src/main/java/com/example/Immigration/Management/System/RESTful/API/Applicant.java
+++ src/main/java/com/example/Immigration/Management/System/RESTful/API/Applicant.java
@@ -3,7 +3,7 @@ package com.example.Immigration.Management.System.RESTful.API;
 import jakarta.persistence.*;
 
 @Entity
-public class Applicant extends Person{
+public class Applicant extends Person {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -14,4 +14,45 @@ public class Applicant extends Person{
 
     private boolean criminalRecord;
 
+    public Applicant() {
+    }
+
+    public Applicant(Long id, String passportNumber, String nationality, boolean criminalRecord) {
+        this.id = id;
+        this.passportNumber = passportNumber;
+        this.nationality = nationality;
+        this.criminalRecord = criminalRecord;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPassportNumber() {
+        return passportNumber;
+    }
+
+    public void setPassportNumber(String passportNumber) {
+        this.passportNumber = passportNumber;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public boolean isCriminalRecord() {
+        return criminalRecord;
+    }
+
+    public void setCriminalRecord(boolean criminalRecord) {
+        this.criminalRecord = criminalRecord;
+    }
 }

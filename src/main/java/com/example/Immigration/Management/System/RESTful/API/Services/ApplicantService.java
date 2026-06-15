8986d413e4ee2d3298d44d93fcda50f4203024cf
Wala AL mawali <wala.salam.9888@gmail.com>
2026-06-15 10:55:45 +0400

    Implement saveApplicant(Applicant applicant) function

--- src/main/java/com/example/Immigration/Management/System/RESTful/API/Services/ApplicantService.java
+++ src/main/java/com/example/Immigration/Management/System/RESTful/API/Services/ApplicantService.java
@@ -1,5 +1,6 @@
 package com.example.Immigration.Management.System.RESTful.API.Services;
 
+import com.example.Immigration.Management.System.RESTful.API.Entities.Applicant;
 import com.example.Immigration.Management.System.RESTful.API.Repository.ApplicantRepository;
 import com.example.Immigration.Management.System.RESTful.API.Repository.InterviewRepository;
 import org.springframework.stereotype.Service;
@@ -14,5 +15,19 @@ public class ApplicantService {
         this.interviewRepository = interviewRepository;
     }
 
+    //object
+    public Applicant saveApplicant(Applicant applicant) {
+
+        if (applicant.getPassportNumber() == null || applicant.getPassportNumber().isEmpty()) {
+            throw new RuntimeException("Passport number cannot be null or empty");
+        }
+
+        if (applicant.getFirstName() == null || applicant.getFirstName().isEmpty()
+                || applicant.getLastName() == null || applicant.getLastName().isEmpty()) {
+            throw new RuntimeException("First name and last name are required");
+        }
+
+        return applicantRepository.save(applicant);
+    }
 
 }
